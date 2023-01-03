0ebed3167206d2790e9a6368778aaf2eb8085619
GabsTiago <gabrielatiagodearaujo@outlook.com>
2023-01-03 10:50:17 -0300

    Renders quizzes on the screen

--- src/scripts/home.js
+++ src/scripts/home.js
@@ -1,3 +1,37 @@
+const BASE_API_URL = "https://mock-api.driven.com.br/api/v6/buzzquizz/quizzes";
+
+getsAllQuizzes();
+
 function goToHomePage() {
   window.location.reload();
+}
+
+function getsAllQuizzes() {
+  axios
+    .get(BASE_API_URL)
+    .then((res) => {
+      const { data } = res;
+      renderQuizzes(data);
+    })
+    .catch((err) => {
+      console.error(err);
+      alert(`Erro ${err.status} - Problema ao carregar quizzes`);
+      window.location.reload();
+    });
+}
+
+function renderQuizzes(allQuizzes) {
+  let quizzesContainer = document.querySelector(".all-quizzes");
+
+  allQuizzes.forEach((quiz) => {
+    const { id, title, image } = quiz;
+
+    quizzesContainer.innerHTML += `
+      <div class="quizz">
+        <img class="quizz-img" src="${image}" />
+        <div class="quizz-overlay"></div>
+        <h5 class="quizz-title">${title}</h5>
+      </div>
+    `;
+  });
 }
