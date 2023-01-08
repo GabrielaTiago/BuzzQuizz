ba53098ed5675c7a8f4d768a6b2f5c7703e3aa10
GabsTiago <gabrielatiagodearaujo@outlook.com>
2023-01-08 14:09:52 -0300

    Shows the user quizzes if they exist

--- src/scripts/home.js
+++ src/scripts/home.js
@@ -1,9 +1,72 @@
 const BASE_API_URL = "https://mock-api.driven.com.br/api/v6/buzzquizz/quizzes";
 
+checksForUserQuizzes();
+buildsAlQuizzesContainerElement();
 getsAllQuizzes();
 
 function goToHomePage() {
   window.location.reload();
+}
+
+function checksForUserQuizzes() {
+  const userQuizzes = localStorage.getItem("userQuizzes");
+  const exitUserQuizzes = JSON.parse(userQuizzes);
+
+  if (exitUserQuizzes) {
+    buildsUserQuizzContainerElement();
+    getsUserQuizzes(exitUserQuizzes);
+  } else {
+    buildsCreateQuizzElement();
+  }
+}
+
+function getsUserQuizzes(userQuizzes) {
+  let userQuizzesContainer = document.querySelector(".all-user-quizzes");
+
+  userQuizzes.forEach((quiz) => {
+    const { id, title, image } = quiz;
+
+    userQuizzesContainer.innerHTML += `
+      <div id="${id}" class="quizz">
+        <img class="quizz-img" src="${image}" />
+        <div class="quizz-overlay"></div>
+        <h5 class="quizz-title">${title}</h5>
+      </div>
+    `;
+  });
+}
+
+function buildsCreateQuizzElement() {
+  const createQuizzContainer = `
+    <div class="create-quiz-container home-page">
+      <h4 class="create-quiz-text">Você não criou nenhum </br> quizz ainda :(</h4>
+      <button class="create-quiz-button" onclick="openNewQuizzPage()">Criar Quiz</button>
+    </div>
+  `;
+  elementConstructor(createQuizzContainer);
+}
+
+function buildsUserQuizzContainerElement() {
+  const userQuizzesContainer = `
+    <div class="quizzes-container home-page user-quizzes-container">
+      <div class="user-quizzes-title">
+        <h3 class="quizzes-title">Seus Quizzes</h3>
+        <i class="fa-solid fa-circle-plus icon add-icon" onclick="openNewQuizzPage()"></i>
+      </div>
+      <div class="all-user-quizzes"></div>
+    </div>
+  `;
+  elementConstructor(userQuizzesContainer);
+}
+
+function buildsAlQuizzesContainerElement() {
+  const allQuizzesContainer = `
+    <div class="quizzes-container home-page">
+      <h3 class="quizzes-title">Todos os Quizzes</h3>
+      <div class="all-quizzes"></div>
+    </div>
+  `;
+  elementConstructor(allQuizzesContainer);
 }
 
 function getsAllQuizzes() {
@@ -34,4 +97,9 @@ function renderQuizzes(allQuizzes) {
       </div>
     `;
   });
+}
+
+function elementConstructor(elementContainer) {
+  const container = document.querySelector("main");
+  container.innerHTML += elementContainer;
 }
